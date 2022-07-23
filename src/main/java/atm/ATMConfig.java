a6faebaacc6fc2672dafd9a7e0b0463bd402b6f5
Teerapat Kuiyawattannon <teerapat.ku@ku.th>
2022-07-23 11:53:37 +0700

    code config

---
+++ src/main/java/atm/ATMConfig.java
@@ -0,0 +1,36 @@
+package atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.crypto.Data;
+
+@Configuration
+
+public class ATMConfig {
+//    @Bean
+//    public DataSource dataSource() {
+//        return new DataSourceDB();
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceFile("customers.txt");
+    }
+    @Bean
+    public Bank bank(){
+        return new Bank("My Bank", dataSource());
+    }
+
+
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+}
